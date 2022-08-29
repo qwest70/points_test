c392364f5dbd5efa3a672ce0495dcf4a00213818
kosov.as <kosov.as@micran.ru>
2022-08-29 14:08:23 +0700

    Тест по граничным значениям

---
+++ boundary_value.py
@@ -0,0 +1,24 @@
+# техника анализа граничных значений(выбираем 100, 101, 200, 201, 500, 501)
+import main
+
+def test_border():
+    count = 0
+    if main.main_menu(score=100) == 1:
+        count += 1
+    if main.main_menu(score=101) == 3:
+        count += 1
+    if main.main_menu(score=200) == 3:
+        count += 1
+    if main.main_menu(score=201) == 5:
+        count += 1
+    if main.main_menu(score=500) == 5:
+        count += 1
+    if main.main_menu(score=501) == 10:
+        count += 1
+    return count
+
+
+if test_border() == 6:
+    print('Test equivalent class - Passed')
+else:
+    print('Test equivalent class - False')
